f966122c49b7cd4b1d40ba03b8f598ed207ae6ea
cheoljun <ljc3484@naver.com>
2022-11-28 11:14:35 +0900

    di: validation, aws / user entity

--- src/main/java/com/example/itDa/domain/model/User.java
+++ src/main/java/com/example/itDa/domain/model/User.java
@@ -1,4 +1,31 @@
 package com.example.itDa.domain.model;
 
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+
+@Entity
 public class User {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @CreatedDate
+    private String createdAt;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column
+    private String profileImg;
+
+    @Column
+    private int density;
+
 }
