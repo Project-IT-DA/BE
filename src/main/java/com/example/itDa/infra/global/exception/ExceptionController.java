e43623e538d75be6eec71bc18b5f8ca69f496d93
cheoljun <ljc3484@naver.com>
2022-11-30 22:02:39 +0900

    add: S3uploader, exceptionHandler

---
+++ src/main/java/com/example/itDa/infra/global/exception/ExceptionController.java
@@ -0,0 +1,15 @@
+package com.example.itDa.infra.global.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExceptionController {
+
+    @GetMapping("/exception")
+    public ResponseEntity<?> exceptionTest() {
+        throw new RequestException(ErrorCode.USER_INFO_NOT_FORMATTED);
+    }
+}
+
